9056a806812b5cc6b9777af8a2eb8f1e20281a38
ClydeH <283jejn04@gmail.com>
2026-01-13 09:58:50 +0800

    Feat: pop() & push()

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ cohort02.unshift("錢二");
 console.log("cohort02.unshift(\"錢二\"): " + cohort02);
 console.log(cohort02 != cohort ? "是否改變原陣列（ mutate ）：是" : "是否改變原陣列（ mutate ）：否");
 console.log("---------------------------------------------------");
+
+// pop()
+console.log("pop()，移除末個陣列元素")
+let cohort03 = ["張三", "李四", "王五"];
+cohort03.pop();
+console.log("cohort03.pop(): " + cohort03);
+console.log(cohort03 != cohort ? "是否改變原陣列（ mutate ）：是" : "是否改變原陣列（ mutate ）：否");
+console.log("---------------------------------------------------");
+
+// push()
+console.log("push(_內容_)，加入末個陣列元素")
+let cohort04 = ["張三", "李四", "王五"];
+cohort04.push("趙六");
+console.log("cohort04.push(\"趙六\"): " + cohort04);
+console.log(cohort04 != cohort ? "是否改變原陣列（ mutate ）：是" : "是否改變原陣列（ mutate ）：否");
+console.log("---------------------------------------------------");
